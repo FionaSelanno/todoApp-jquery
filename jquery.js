38edae09ab983ff66f2f272ae5de42e77c1812af
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-17 15:37:30 +0100

    get input from user and add it to the list

--- jquery.js
+++ jquery.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-  $('h1').append('<p>something here</p>');
+    $('#addItem').click(function(){ //when button is clicked do what's in the function
+        var input = $('input').val(); //takes the value of input
+        $('.listToDo').append('<div class="item">' + input +'</div>'); //appends the value of input to .listToDo
+   });
 });
