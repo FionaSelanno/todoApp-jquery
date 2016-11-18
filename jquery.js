c93d32152ab8c33cbfae0c2ab0d7dd22d11c89ef
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-18 14:06:21 +0100

    cross item of list

--- jquery.js
+++ jquery.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
+
+    // add items:
     $('#addItem').click(function(){ //when button is clicked do what's in the function
         var $input = $('input').val(); //takes the value of input
-        $('.listToDo').append('<input class="item" type="checkbox">' + $input +'</div>'); //appends the value of input to .listToDo as an input with a checkbox 
+        $('.listToDo').append('<div class="item">' + $input + '</div>'); //appends the value of input to .listToDo as an input with a checkbox
    });
+
+   // cross item if list:
+   $(document).on('click', '.item', function(){
+     $(this).css('text-decoration', 'line-through');
+   });
+
 });
