030057cdb7ab89af805a4103849afc570adee1bb
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-18 12:39:55 +0100

    add items als inputs to the list

--- jquery.js
+++ jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#addItem').click(function(){ //when button is clicked do what's in the function
-        var input = $('input').val(); //takes the value of input
-        $('.listToDo').append('<div class="item">' + input +'</div>'); //appends the value of input to .listToDo
+        var $input = $('input').val(); //takes the value of input
+        $('.listToDo').append('<input class="item" type="checkbox">' + $input +'</div>'); //appends the value of input to .listToDo as an input with a checkbox 
    });
 });
