b2c84ff9d06268201e6293b5e3c5a43add49748d
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-21 12:13:13 +0100

    counting items todo

--- jquery.js
+++ jquery.js
@@ -5,18 +5,28 @@ $(document).ready(function(){
         var $input = $('input').val(); //takes the value of input
         $('.listToDo').append('<li class="item"><input type="checkbox">' + $input + '</li>'); //appends the value of input to .listToDo as an input with a checkbox
 
-        //counting total items on list
-        var $totalItems = $(".item").length;
-        $('#total').html($totalItems);
+        countingTotals();
    });
 
    // cross item of list:
     $(document).on('click', '.item', function(){
       var $itemDone= $(this).toggleClass('itemDone');
 
-      //counting total items that are crossed off the list
-      var $totalDone = $('.itemDone').length;
-      $('#totalDone').html($totalDone);
-
+      countingTotals();
    });
+
+// put $totalItems and $totalDone in a separate function to be able to calculate $totalToDo. Then call this function after an item is added or crossed off the list. See r8,15.
+   function countingTotals(){
+     //counting total items on list
+     var $totalItems = $(".item").length;
+     $('#total').html($totalItems);
+
+     //counting total items that are crossed off the list
+     var $totalDone = $('.itemDone').length;
+     $('#totalDone').html($totalDone);
+
+     //counting total todo items
+     var $totalToDo = $totalItems - $totalDone;
+     $('#totalToDo').html($totalToDo);
+   }
 });
