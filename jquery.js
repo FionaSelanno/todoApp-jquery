3c97b5a4a7d9eda19c53174a8300613399ac1b66
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-21 11:38:52 +0100

    counting total items done

--- jquery.js
+++ jquery.js
@@ -2,19 +2,21 @@ $(document).ready(function(){
 
     // add items:
     $('#addItem').click(function(){ //when button is clicked do what's in the function
-        // var $list = [];
         var $input = $('input').val(); //takes the value of input
-        $('.listToDo').append('<li class="item">' + $input + '</li>'); //appends the value of input to .listToDo as an input with a checkbox
+        $('.listToDo').append('<li class="item"><input type="checkbox">' + $input + '</li>'); //appends the value of input to .listToDo as an input with a checkbox
 
-    //counting total items on list
+        //counting total items on list
         var $totalItems = $(".item").length;
-        $('.total').html($totalItems);
+        $('#total').html($totalItems);
    });
 
-   // cross item if list:
-   $(document).on('click', '.item', function(){
-     $(this).css('text-decoration', 'line-through');
+   // cross item of list:
+    $(document).on('click', '.item', function(){
+      var $itemDone= $(this).toggleClass('itemDone');
+
+      //counting total items that are crossed off the list
+      var $totalDone = $('.itemDone').length;
+      $('#totalDone').html($totalDone);
+
    });
-
-
 });
