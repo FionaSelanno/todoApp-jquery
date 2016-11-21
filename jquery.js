4cdc3d7189284f216821f7ce1d02ab0bb24ae842
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-21 22:09:01 +0100

    first styling

--- jquery.js
+++ jquery.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   // add items:
   $('#addItem').click(function(){ //when button is clicked do what's in the function
     var $input = $('input').val(); //takes the value of input
-    $('.listToDo').append('<li class="item"><input type="checkbox">' + $input + '</li>'); //appends the value of input to .listToDo as an input with a checkbox
+    $('.listToDo').append('<li class="item">' + $input + '</li>'); //appends the value of input to .listToDo as an input with a checkbox
 
       countingTotals();
   });
